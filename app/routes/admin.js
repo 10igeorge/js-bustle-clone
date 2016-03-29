f658866a87f155d2dba6b11bd317cc7f247aa02a
Isabelle George and Matt Rosanio <mmrosani@syr.edu>
2016-03-29 10:28:43 -0700

    Add functionality to add new articles

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,5 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(){
     return this.store.findAll('article');
+  },
+
+  actions: {
+    submitArticle(params){
+      var newArticle = this.store.createRecord('article', params);
+      newArticle.save();
+      this.transitionTo('admin');
+    },
+    edit(){
+
+    },
+    update(){
+
+    }
   }
 });
